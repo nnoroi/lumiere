bf42f23664d1b7346328214a8ce01d25504c1380
Dymytrii <barbotkodim@gmail.com>
2026-06-25 10:30:51 +0100

    Add movie details page

--- server.js
+++ server.js
@@ -13,6 +13,14 @@ app.get('/', (req, res) => {
     res.render('index', { movies: moviesData }); 
 });
 
+app.get("/booking", (req,res) => {
+  const movieId = req.query.movieId;
+  const selectedMovie = moviesData.find(m => m.id === movieId || m.id === parseInt(movieId)); // checks if the movie exists in json
+  if (!selectedMovie) {
+    return res.status(404).send("Movie not found");
+  }
+  res.render('booking', {movie: selectedMovie});
+})
 app.listen(3000, () => {
   console.log('Express server running at http://localhost:3000/');
 });
